caebbebf326dab86e1d2dd53fa0ece23b94fb829
jifnotgif <david.doo@stonybrook.edu>
2018-02-16 17:33:08 -0500

    TTT recitation 3.

---
+++ TicTacToe/src/ttt/gui_better/TTTBoard.java
@@ -0,0 +1,51 @@
+package ttt.gui_better;
+
+import javafx.scene.layout.ColumnConstraints;
+import ttt.engine.TTTEngine;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
+
+/**
+ * Graphical Tic-Tac-Toe board.
+ *
+ * @author Eugene Stark
+ * @version 20180211
+ */
+public class TTTBoard extends GridPane {
+
+    private final TTTAppGUI gui;
+    private final TTTEngine engine;
+    private final int dim;
+
+
+    public TTTBoard(TTTAppGUI app, TTTEngine engine) {
+        this.gui = app;
+        this.engine = engine;
+        this.dim = engine.getDim();
+        setConstraints();
+        addButtons();               
+    }
+
+    private void setConstraints() {
+        for (int i = 0; i < dim; i++) {
+            RowConstraints rc = new RowConstraints();
+            rc.setVgrow(Priority.ALWAYS);
+            rc.setFillHeight(true);
+            this.getRowConstraints().add(rc);
+            ColumnConstraints cc = new ColumnConstraints();
+            cc.setHgrow(Priority.ALWAYS);
+            cc.setFillWidth(true);
+            this.getColumnConstraints().add(cc);
+        }
+    }
+
+    private void addButtons() {
+        for(int i = 0; i < dim; i++) {
+            for(int j = 0; j < dim; j++) {
+                this.add(new TTTButton(gui, engine, i, j), i , j);
+            }
+        }
+    }
+
+}
---
+++ TicTacToe/src/ttt/gui_better/TTTButton.java
@@ -0,0 +1,49 @@
+package ttt.gui_better;
+
+import ttt.engine.TTTEngine;
+import javafx.scene.control.Button;
+
+/**
+ * Button for graphical Tic-Tac-Toe board.
+ *
+ * @author Eugene Stark
+ * @version 20180211
+ */
+public class TTTButton extends Button {
+
+    private static final int PREF_SIZE = 50;
+
+    private final int row, col;
+    private final TTTAppGUI gui;
+    private final TTTEngine engine;
+
+    public TTTButton(TTTAppGUI app, TTTEngine engine, int row, int col) {
+        this.gui = app;
+        this.engine = engine;
+        this.row = row;
+        this.col = col;             
+        this.setPrefWidth(PREF_SIZE);
+        this.setPrefHeight(PREF_SIZE);
+        this.setMaxWidth(Double.MAX_VALUE);
+        this.setMaxHeight(Double.MAX_VALUE);
+        
+            setListener();
+            }
+
+    private void setListener() {
+        this.setOnAction(e -> {
+            try {
+                this.setText(engine.getPlayerToMove()
+                        == TTTEngine.X_PLAYER ? "X" : "O");
+                engine.makeMove(row, col);
+                if (engine.gameOver()) {
+                    gui.alertGameOver();
+                }
+            } catch (TTTEngine.IllegalMoveException x) {
+                gui.alertIllegalMove(x);
+            }
+        });
+
+    }
+
+}
